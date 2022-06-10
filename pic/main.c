83dbce5a063ee553f7bd4f0942a28c5ee6d55065
Davide <viottodavide00@outlook.it>
2022-06-10 12:10:25 +0200

    Send array

--- pic/main.c
+++ pic/main.c
@@ -11,33 +11,54 @@
 #include <xc.h>
 #define _XTAL_FREQ 16000000
 
+char picId = 0x01;
+int placeHolder = 768;
 void init_UART(int);
+void send_Byte(char);
+void send_Array(char *);
 
 void main(void) {
-    //TRISB = 0x00;
-    //PORTB = 0x00;
+    TRISB = 0x00;
     init_UART(9600);
     while(1){
+        char str[] = "prova";
         __delay_ms(500);
-        while(TXIF);
-        TXREG = 'a';
-        while(!(TXSTA & 0x02));
-
+        send_Array(str);
     }
 
-    
 }
 
+void send_Byte(char byte)
+{
+        if(TXIF);
+        TXREG = byte;
+}
+
+void send_Array(char * array)
+{
+    static char pos = 0;
+
+    if(TXIF)
+    {
+        send_Byte(array[pos]);
+        PORTB = array[pos];
+        if(array[pos] == '\0')
+            pos = 0;
+        else
+            pos++;
+    }
+}
 void init_UART(int baudRate)
 {
     // Interrupt configuration
     INTCON = 0xC0;
     PIE1 |= 0x02;
-    
     TRISC |= 0x80; // set RC7 to input (RX)
     TRISC &= !0x40; // RC6 to output (TX)
+
     TXSTA = 0x24;
     RCSTA |= 0x80;
+
     SPBRG = (_XTAL_FREQ/(long)(64UL*baudRate))-1;
 }
 
