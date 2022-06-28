4bd26a656a3145d5a505fad1e1c7e2bafb1ea0e2
davide <davide@davide-MS-7A34>
2022-06-28 13:36:04 +0200

    Fixed more stuff

--- pic/main.c
+++ pic/main.c
@@ -18,14 +18,21 @@
 
 // Register settings
 
-#define UART_INTCON 0xC0    // Global and peripheral interrupt enabled
-#define UART_PIE1 0x22      //
-
 typedef struct {
-	unsigned int msTrig : 1;
-}valReg;
+	unsigned int bit0 : 1;
+    unsigned int bit1 : 1;
+    unsigned int bit2 : 1;
+    unsigned int bit3 : 1;
+    unsigned int bit4 : 1;
+    unsigned int bit5 : 1;
+    unsigned int bit6 : 1;
+    unsigned int bit7 : 1;
+}boolReg;
 
-valReg valReg1;
+boolReg valReg1;
+/*
+ * 0 = millis trigger
+ */
 char picId = 0x01;
 int placeHolder = 768;
 
@@ -34,94 +41,46 @@ int seconds = 0;
 
 char dataArray[64];
 
-char boolReg = 0;
-/*
- * 1 = device is sending data
- * 2 = send trigger state
- * 8 = millis interrupt
- */
-
-void send_byte(char);
-void send_array(char *);
-void timer_handler(void);
+void system_init(void);
 void com_handler(void);
 char id_request(void);
-
-void system_init(void);
-void uart_init(int);
-void timer_init(void);
 void adc_init(void);
+
+// Serial communication functions
+void uart_init(int);
+void send_byte(char);
+char send_array(char *);
+
+// Timer functions
+void timer_init(void);
+void timer_handler(void);
 
 void main(void) {
     system_init();
     while(1)
     {
-        char str[] = "prova";
-        send_array(str);
+        timer_handler();
+        com_handler();
     }
 }
 
-void send_byte(char byte)
+void __interrupt() ISR()
 {
-    if(PIR1bits.TXIF)
-        PORTD = byte;
-        TXREG = byte;
-}
 
-void send_array(char * array)
-{
-    static char pos = 0;
-    boolReg |= 0x01;
+    if (PIR1bits.RCIF)  // Interrupt RX
+    {
+        PORTD = RCREG;
+        PIR1bits.RCIF = 0;
+    }
     
-    if(PIR1bits.TXIF)
+    if (INTCONbits.TMR0IF)  // Interrupt timer
     {
-        PORTB = array[pos];
-        send_byte(array[pos]);
-        // PORTB = array[pos];
-        if(array[pos] == '\0')
-        {
-            pos = 0;
-            boolReg &= !0x01;
-        }
-        else
-
-            pos++;
-    }
-}
-
-void timer_handler()
-{
-    if(valReg1.msTrig)
-    {
-        if(++millis >= 1000)
-        {
-            millis = 0;
-            seconds++;
-        }
-        
-        valReg1.msTrig = 0;
-    }
-}
-
-void com_handler()
-{   
-    if((!(seconds % UPD_DELAY) && (boolReg & 0x02)) || boolReg & 0x01)
-    {
-        char str[] = "prova";
-        send_array(str);
+        valReg1.bit0 = 1;
+        INTCONbits.TMR0IF = 0;
     }
 
-    if(!(seconds % UPD_DELAY))
-        boolReg &= ~0x02;
-    else
-        boolReg |= 0x02;
-
 }
 
-char id_request()
-{
-    
-}
 void system_init()
 {
     TRISB = 0x00;
@@ -130,7 +89,35 @@ void system_init()
     INTCONbits.GIE = 1;     // Enable global interrupt
     INTCONbits.PEIE = 1;    // Enable peripheral interrupt 
     uart_init(9600);
+    timer_init();
 }
+
+
+void com_handler()
+{   
+    char str[] = "prova";
+    static boolReg comReg;
+    
+    if((!(seconds % UPD_DELAY) && comReg.bit0) || comReg.bit1)
+    {
+        comReg.bit1 = send_array(str);
+    }
+
+    if(!(seconds % UPD_DELAY))
+        comReg.bit0 = FALSE;
+    else
+         comReg.bit0 = TRUE;
+    
+}
+
+char id_request()
+{
+    
+}
+
+/*
+ * Serial communication functions
+ */
 
 void uart_init(int baudrate)
 {
@@ -145,35 +132,62 @@ void uart_init(int baudrate)
 
     TXSTAbits.TXEN = 1;     // Enable transmission
     TXSTAbits.BRGH = 1;     //Enable Hight speed
-    
-   
-
 
     SPBRG = (_XTAL_FREQ/(long)(64UL*baudrate))-1;
 }
 
+void send_byte(char byte)
+{
+    if(PIR1bits.TXIF)
+        PORTD = byte;
+        TXREG = byte;
+}
+
+char send_array(char * array)
+{
+    static char pos = 0;
+    if(PIR1bits.TXIF)
+    {
+        PORTB = array[pos];
+        send_byte(array[pos]);
+        // PORTB = array[pos];
+        if(array[pos] == '\0')
+        {
+            pos = 0;
+            return FALSE;
+        }
+        else
+            pos++;
+        return TRUE;
+    }   
+}
+
+/*
+ * Timer functions
+ */
+
 void timer_init()
 {
-
+    // Timer is configured to produce an interrupt ever 1ms at 8Mhz
+    
+    
     INTCONbits.TMR0IE = 1;      // Enable TIMER 0 interrupt
     OPTION_REGbits.T0CS = 0;    // Select internal clock
     OPTION_REGbits.PS1 = 1;     // Prescaler to 1:8
     TMR0 = 6;                   // Preload
 }
 
-void __interrupt() ISR()
+void timer_handler()
 {
-
-    if (PIR1bits.RCIF)  // Interrupt RX
+    if(valReg1.bit0)
     {
-        PORTD = RCREG;
-        PIR1bits.RCIF = 0;
+        if(++millis >= 1000)
+        {
+            millis = 0;
+            seconds++;
+        }
+        
+        valReg1.bit0 = 0;
     }
-    
-    if (INTCONbits.TMR0IF)  // Interrupt timer
-    {
-        valReg1.msTrig = 1;
-        INTCONbits.TMR0IF = 0;
-    }
-
 }
+
